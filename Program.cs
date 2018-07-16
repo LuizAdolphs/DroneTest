9a6080f6ee037c244e4b02d782f83042816b84ac
Luiz Adolphs <luiz.adolphs@gmail.com>
2018-07-16 00:38:56 -0300

    First alpha version. TODO: Separate classes in different files and pass all tests

--- Program.cs
+++ Program.cs
@@ -39,62 +39,75 @@ namespace Algorithm.Logic
         /// <returns>String representando o ponto cartesiano após a execução dos comandos (X, Y)</returns>
         public static string Evaluate(string input)
         {
-            // TODO: Este método é o ponto de entrada para a lógica.
-            return "(X, Y)";
+            var commandProcessor = new CommandsProcessor(input);
+
+            return commandProcessor.Process();
         }
 
-
-        public enum DirectionsStep
+        public enum CommandsStep
         {
             Begin,
-            VerifStep,
+            VerifyStep,
             AddStep,
             AddRepeats,
+            CancelLastOperation,
+            IncreaseIndex,
             End
         }
 
-
-
-        public class DirectionsProcessor
+        public class CommandsProcessor
         {
 
-            private Dictionary<DirectionsStep, IDirectionStep> _directionSteps = new Dictionary<DirectionsStep, IDirectionStep>
+            private Dictionary<CommandsStep, ICommandStep> _commandSteps = new Dictionary<CommandsStep, ICommandStep>
             {
-                { DirectionsStep.Begin, new VerifyPositionInCommandStep() },
-                { DirectionsStep.VerifStep, new VerifyPositionInCommandStep() }
+                { CommandsStep.Begin, new VerifyNextStepInCommandStep() },
+                { CommandsStep.VerifyStep, new VerifyNextStepInCommandStep() },
+                { CommandsStep.AddStep, new AddNewCommandStep() },
+                { CommandsStep.CancelLastOperation, new CancelLastCommandStep() },
+                { CommandsStep.AddRepeats, new AddNumberToRepeatStep() },
+                { CommandsStep.IncreaseIndex, new IncreaseIndexStep() }
             };
 
+            private string _command = string.Empty;
 
-            public string Process(string command)
+            public CommandsProcessor(string command)
             {
-                var stepCollection = new StepCollection(command);
-                var step = DirectionsStep.Begin;
+                this._command = command;
+            }
 
-                while (step != DirectionsStep.End)
+            public string Process()
+            {
+                var stepCollection = new StepCollection(this._command);
+                var step = CommandsStep.Begin;
+
+                do
                 {
-                    _directionSteps[step].Execute(stepCollection, ref step);
+                    _commandSteps[step].Execute(stepCollection, ref step);
                 }
+                while (step != CommandsStep.End);
 
-                return "";
+                var output = stepCollection.GenerateOutput();
+
+                return $"({output.Item1}, {output.Item2})";
             }
         }
 
         public struct Step
         {
-            public char Direction { get; set; }
+            public char Command { get; set; }
             public int Repeat { get; set; }
 
-            public Step(char direction, int repeat = 1)
+            public Step(char Command, int repeat = 1)
             {
-                this.Direction = direction;
+                this.Command = Command;
                 this.Repeat = repeat;
             }
         }
 
         public class StepCollection
         {
-            public IList<Step> Steps { get; set; } = new List<Step>();
-            public int Index { get; set; } = 0;
+            private IList<Step> _steps = new List<Step>();
+            public int Index { get; private set; } = 0;
             
             public string Command { get; }
 
@@ -107,40 +120,167 @@ namespace Algorithm.Logic
             {
                 return this.Command[this.Index];
             }
-        }
 
-        public interface IDirectionStep
-        {
-            StepCollection Execute(StepCollection stepCollection, ref DirectionsStep currentStep);
-        }
-
-        public abstract class StepBase
-        {
-            protected char[] _directions = new char[] { 'N', 'L', 'S', 'O' };
-        }
-
-        public class VerifyPositionInCommandStep : StepBase, IDirectionStep
-        {
-            public StepCollection Execute(StepCollection stepCollection, ref DirectionsStep currentStep)
+            public StepCollection IncreaseIndex()
             {
-                var currentChar = stepCollection.CharAtIndex();
+                this.Index++;
 
-                if (_directions.Contains(currentChar))
+                return this;
+            }
+
+            public StepCollection AddStep(Step step)
+            {
+                this._steps.Add(step);
+
+                return this;
+            }
+
+            public StepCollection RemoveLatestStep()
+            {
+                this._steps.RemoveAt(this._steps.Count - 1);
+
+                return this;
+            }
+
+            public StepCollection AddNumberToLatestStepRepeat(char number)
+            {
+                var last = this._steps.Last();
+                last.Repeat = Convert.ToInt32(String.Concat(this._steps.Last().Repeat, number));
+
+                return this;
+            }
+
+            public bool VerifyIfCommandTerminated()
+            {
+                return this.Index > this.Command.Length - 1;
+            }
+
+            public Tuple<int, int> GenerateOutput()
+            {
+                return this._steps.Aggregate<Step, Tuple<int, int>>(new Tuple<int, int>(0,0), (accumulated, step) =>
                 {
-                    currentStep = DirectionsStep.AddStep;
+                    int directionQuantity;
+
+                    if (step.Command.Equals('N'))
+                    {
+                        directionQuantity = accumulated.Item1 + step.Repeat;
+                        return new Tuple<int, int>(directionQuantity, accumulated.Item2);
+                    }
+
+                    if (step.Command.Equals('S'))
+                    {
+                        directionQuantity = accumulated.Item1 - step.Repeat;
+                        return new Tuple<int, int>(directionQuantity, accumulated.Item2);
+                    }
+
+                    if (step.Command.Equals('L'))
+                    {
+                        directionQuantity = accumulated.Item2 + step.Repeat;
+                        return new Tuple<int, int>(accumulated.Item1, directionQuantity);
+                    }
+
+                    if (step.Command.Equals('O'))
+                    {
+                        directionQuantity = accumulated.Item2 - step.Repeat;
+                        return new Tuple<int, int>(accumulated.Item1, directionQuantity);
+                    }
+
+                    return accumulated;
+                });
+
+            }
+        }
+
+        public interface ICommandStep
+        {
+            StepCollection Execute(StepCollection stepCollection, ref CommandsStep currentStep);
+        }
+
+        public class VerifyNextStepInCommandStep : ICommandStep
+        {
+            private char[] _commands = new char[] { 'N', 'L', 'S', 'O' };
+            private char cancelLastOperation = 'X';
+
+            public StepCollection Execute(StepCollection stepCollection, ref CommandsStep currentStep)
+            {
+
+                if (stepCollection.VerifyIfCommandTerminated())
+                {
+                    currentStep = CommandsStep.End;
+                }
+                else
+                {
+
+                    var currentChar = stepCollection.CharAtIndex();
+
+                    if (_commands.Contains(currentChar))
+                    {
+                        currentStep = CommandsStep.AddStep;
+                    }
+                    else if (currentChar.Equals(cancelLastOperation))
+                    {
+                        currentStep = CommandsStep.CancelLastOperation;
+                    }
+                    else
+                    {
+                        if (int.TryParse(currentChar.ToString(), out int numericChar))
+                        {
+                            currentStep = CommandsStep.AddRepeats;
+                        }
+                        else
+                        {
+                            currentStep = CommandsStep.IncreaseIndex;
+                        }
+                    }
                 }
 
                 return stepCollection;
             }
         }
 
-        public class AddNewDirectionStep : StepBase, IDirectionStep
+        public class IncreaseIndexStep : ICommandStep
         {
-            public StepCollection Execute(StepCollection stepCollection, ref DirectionsStep currentStep)
+            public StepCollection Execute(StepCollection stepCollection, ref CommandsStep currentStep)
             {
-                stepCollection.Steps.Add(new Step(stepCollection.CharAtIndex()));
+                stepCollection.IncreaseIndex();
 
-                currentStep = DirectionsStep.VerifStep;
+                currentStep = CommandsStep.VerifyStep;
+
+                return stepCollection;
+            }
+        }
+
+        public class AddNewCommandStep : ICommandStep
+        {
+            public StepCollection Execute(StepCollection stepCollection, ref CommandsStep currentStep)
+            {
+                stepCollection.AddStep(new Step(stepCollection.CharAtIndex()));
+
+                currentStep = CommandsStep.IncreaseIndex;
+                                
+                return stepCollection;
+            }
+        }
+
+        public class CancelLastCommandStep : ICommandStep
+        {
+            public StepCollection Execute(StepCollection stepCollection, ref CommandsStep currentStep)
+            {
+                stepCollection.RemoveLatestStep();
+
+                currentStep = CommandsStep.IncreaseIndex;
+
+                return stepCollection;
+            }
+        }
+
+        public class AddNumberToRepeatStep : ICommandStep
+        {
+            public StepCollection Execute(StepCollection stepCollection, ref CommandsStep currentStep)
+            {
+                stepCollection.AddNumberToLatestStepRepeat(stepCollection.CharAtIndex());
+
+                currentStep = CommandsStep.IncreaseIndex;
 
                 return stepCollection;
             }
