9ee055ac3610a99746030b1993f32f62da2b4f76
Adolphs, Luiz - Dell Team <L_Adolphs@Dellteam.com>
2018-07-16 20:43:15 -0300

    Second tentative

--- .after_test.cmd
+++ .after_test.cmd
@@ -3,6 +3,6 @@ nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
  
 nuget install coveralls.net -Version 0.7.0 -OutputDirectory tools
 
-./tools/OpenCover.4.6.519/tools/OpenCover.Console.exe -target:.tools/NUnit.ConsoleRunner.3.8.0/tools/nunit3-console.exe -targetargs:"/nologo /noshadow ./Algorithm.Logic.Tests/bin/Debug/Algorithm.Logic.CommandProcessor.dll" -filter:"+[*]* -[*.Tests]*" -register:user
+.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe -targetargs:".\Algorithm.Logic.Tests\bin\Debug\Algorithm.Logic.Tests.dll -noheader" -filter:"+[*]* -[*.Tests]*" -register:user
  
-./tools/coveralls.net.0.7.0/tools/csmacnz.Coveralls.exe --opencover -i ./results.xml
+.\tools\coveralls.net.0.7.0\tools\csmacnz.Coveralls.exe --opencover -i ./results.xml
